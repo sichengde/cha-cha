25a0db57e45d226dde465c8dc4b1b770084e611b
发展中心mac <2651283633@qq.com>
2026-03-17 17:32:23 +0800

    取消304缓存

--- cha-cha-server/src/app.js
+++ cha-cha-server/src/app.js
@@ -28,6 +28,13 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
+app.use('/api/', function(req, res, next) {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  res.set('Pragma', 'no-cache')
+  res.set('Expires', '0')
+  next()
+})
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 1000,
