64ce893c1128e0e87d2d7272e5e91a6e29ecbae8
越秀 <2651283633@qq.com>
2026-03-22 22:48:51 +0800

    删除很多无用文件，希望正确

--- cha-cha-server/src/database/clear_data.sql
+++
@@ -1,9 +0,0 @@
--- 清空所有历史数据
-DELETE FROM signatures;
-DELETE FROM query_records;
-DELETE FROM query_data;
-DELETE FROM query_headers;
-DELETE FROM query_pages;
-DELETE FROM users;
-
-SELECT '已清空所有数据' as status;
