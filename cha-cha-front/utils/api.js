d9d6bd1ab444f8fca3aed5d1353203e551592ea1
发展中心mac <2651283633@qq.com>
2026-03-23 17:23:04 +0800

    优化设置

--- cha-cha-front/utils/api.js
+++ cha-cha-front/utils/api.js
@@ -5,7 +5,7 @@ var BASE_URL = (function() {
   try {
     var accountInfo = wx.getAccountInfoSync()
     var envVersion = (accountInfo && accountInfo.miniProgram && accountInfo.miniProgram.envVersion) || 'release'
-    if (envVersion === 'develop' || envVersion === 'trial') {
+    if (envVersion === 'develop') {
       return 'http://10.3.4.59:3000'  // 开发/体验环境，可按需替换为测试服务器地址
     }
     return 'https://wx.sygdsoft.com'
